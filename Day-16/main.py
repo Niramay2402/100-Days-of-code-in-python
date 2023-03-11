4fda7c82ab797aa1a9e0894d4982668f5162b660
Niramay2402 <76899983+Niramay2402@users.noreply.github.com>
2023-03-11 16:29:23 +0530

    back

---
+++ Day-16/main.py
@@ -0,0 +1,9 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander", "Bulbasaur"])
+table.add_column("Type", ["Electric", "Water", "Fire", "Grass and Poison"])
+table.align = 'l'
+print(table)
+
