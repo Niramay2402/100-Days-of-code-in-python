1783963b2a59823987a1215370d530e7dc6b9156
Niramay2402 <76899983+Niramay2402@users.noreply.github.com>
2023-04-22 23:58:54 +0530

    Day - 22

---
+++ Day-22/Scoreboard.py
@@ -0,0 +1,12 @@
+from  turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("White")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        
